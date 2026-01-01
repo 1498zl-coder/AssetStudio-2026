f9e70825d0c9bac248401a98d97bccf8522cafe3
Neptune <595554963@qq.com>
2026-01-02 05:14:41 +0800

    Add files via upload
    汉化

---
+++ AssetStudio.FBXWrapper/FbxDll.cs
@@ -0,0 +1,10 @@
+namespace AssetStudio.FbxInterop
+{
+    internal static class FbxDll
+    {
+
+        internal const string DllName = "AssetStudio.FBXNative";
+        internal const string FbxsdkDllName = "libfbxsdk";
+
+    }
+}
